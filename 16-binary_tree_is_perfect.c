9ff62f89959d5549e13e3a31d0eea6d31a898cd8
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-11-01 19:44:27 +0300

    function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -18,9 +18,10 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (!tree->left && !tree->right)
 		return (1);
-
-	return (tree->left && tree->right && binary_tree_is_perfect(tree->left) &&
-			binary_tree_is_perfect(tree->right) && height % 2 == 0);
+	return (tree->left && tree->right &&
+			binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right) &&
+			height == 0);
 }
 
 /**
