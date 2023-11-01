002fa89ffa5d5437cd41c360c843d6c5148443ee
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-11-01 20:35:51 +0300

    removing a trailing whitespace from the code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node->parent->left == node)
 		return (node->parent->right);
-	
+
 	else if (node->parent->right == node)
 		return (node->parent->left);
 
