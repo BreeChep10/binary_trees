8a2ec71b34a655760f60861bba9e4bde297e5861
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-11-01 19:47:44 +0300

    correcting a code fail

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,7 +21,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	return (tree->left && tree->right &&
 			binary_tree_is_perfect(tree->left) &&
 			binary_tree_is_perfect(tree->right) &&
-			height == 0);
+			height % 2 == 0);
 }
 
 /**
