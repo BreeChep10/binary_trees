04f6925c72511dde263cef3a43b8119f1223a04d
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2023-11-02 19:33:17 +0300

    finding an ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_ancestor - finds a common parent between
+ *two nodes else returns null of no common parent is found
+ *
+ *@first: the first node
+ *@second: the second node
+ *Return: the common parent node else NULL
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	binary_tree_t *mom1, *mom2;
+
+	if (!first || !second)
+		return (NULL);
+
+	if (first == second)
+		return ((binary_tree_t *)first);
+
+	mom1 = first->parent;
+	mom2 = second->parent;
+
+
+	if (first == mom2 || (!mom1->parent && mom2) || !mom1)
+		return (binary_trees_ancestor(first, mom2));
+	else if (second == mom1 || (!mom2->parent && mom1) || !mom2)
+		return (binary_trees_ancestor(mom1, second));
+	return (binary_trees_ancestor(mom1, mom2));
+}
